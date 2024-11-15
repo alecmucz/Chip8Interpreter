dd9b92735509e6102e7655e850642282d343a76f
amucz0 <alecmucz@gmail.com>
2024-11-14 22:34:25 -0500

    Implemented Simple Opcodes

--- src/Chip8.cpp
+++ src/Chip8.cpp
@@ -17,9 +17,12 @@ void Chip8::init() {
     soundTimer = 0;
     pc = 0x200;
 
+    srand(time(nullptr)); // NOLINT(*-msc51-cpp)
+
     for (int j = 0; j < sizeof(fonts) / sizeof(fonts[0]); j++){
         memory[j + 0x050] = fonts[j];
     }
+
 }
 
 [[noreturn]] void Chip8::executionCycle() {
@@ -30,73 +33,136 @@ void Chip8::init() {
         pc+=2;
         // *DECODE*
         opcode = inst & 0xF000;
-        uint8_t X = inst & 0x0F00;
-        uint8_t Y = (inst & 0x00F0) >> 8;
-        uint8_t N = (inst & 0x000F) >> 4;
-        uint8_t NN = inst & 0x00FF;
+        uint8_t x = (inst & 0x0F00) >> 8;
+        uint8_t y = (inst & 0x00F0) >> 4;
+        uint8_t n = inst & 0x000F;
+        uint8_t nn = inst & 0x00FF;
+        uint8_t nnn = inst & 0x0FFF;
         // *EXECUTE*
         switch (opcode) {
             case 0x0000:    //system calls
-
+                switch (nn) {
+                    case 0x00E0:    // CLS : Clear the Display
+                        memset(display,0,sizeof(display));
+                        break;
+                    case 0x00EE:    // RET : Return from a Subroutine
+                        pc = stack[sp--];
+                        break;
+                    default:
+                        memDump();
+                        exit(1);
+                }
                 break;
-            case 0x1000:    //jump
-
+            case 0x1000:    // JP : Jump to Location nnn
+                pc = nnn;
                 break;
-            case 0x2000:    //subroutine
-
+            case 0x2000:    //CALL : Call Subroutine at nnn
+                stack[sp++] = pc;
+                pc = nnn;
                 break;
             case 0x3000:    //Conditional Skip
-
+                if (gpr[x] == gpr[nn]){
+                    pc+=2;
+                }
                 break;
             case 0x4000:    //Conditional Skip
-
+                if (gpr[x] != gpr[nn]){
+                    pc+=2;
+                }
                 break;
             case 0x5000:
-
+                if (gpr[x] == gpr[y]){
+                    pc+=2;
+                }
                 break;
             case 0x6000:    //Register Operations
-
+                gpr[x] = nn;
                 break;
-            case 0x7000:    //Register Operations
-
+            case 0x7000:    //Add Operation
+                gpr[x] += nn;
                 break;
             case 0x8000:    //Arithmetic And BitWise Operations
-
+                switch (n) {
+                    case 0x0000:    //8xy0
+                        gpr[x] = gpr[y];
+                        break;
+                    case 0x0001:    //8xy1
+                        gpr[x] = gpr[x] | gpr[y];
+                        break;
+                    case 0x0002:    //8xy2
+                        gpr[x] = gpr[x] & gpr[y];
+                        break;
+                    case 0x0003:    //8xy3
+                        gpr[x] = gpr[x] ^ gpr[y];
+                        break;
+                    default:
+                        memDump();
+                        exit(1);
+                }
                 break;
             case 0xA000:    //Set Index Register
-
+                i = nnn;
                 break;
             case 0xB000:    //Jump with Offset
-
+                pc = nnn + gpr[0];
                 break;
             case 0xC000:    //Random Number (mask)
-
+                gpr[x] = uint8_t(rand()) & nn;
                 break;
             case 0xD000:    //Draw Sprite
 
                 break;
             case 0xE000:    //Key Operations
 
+
                 break;
             case 0xF000:    //Timer Sound Memory Operations
+                switch (nn) {
+                    case 0x0007:
+                        gpr[x] = delayTimer;
+                        break;
+                    case 0x000A:    //implement busy waiting for a keypress and store the value in gpr[x] ; shit implementation, needs fixing
 
+                        break;
+                    case 0x0015:
+                        delayTimer = gpr[x];
+                        break;
+                    case 0x0018:
+                        soundTimer = gpr[x];
+                        break;
+                    case 0x001E:
+                        i += gpr[x];
+                        break;
+                    case 0x0055:
+                        for(int index = 0; index <= x; index++){
+                            memory[i + index] = gpr[index];
+                        }
+                        break;
+                    case 0x0065:
+                        for(int index = 0; index <= x; index++){
+                            gpr[index] = memory[i + index];
+                        }
+                        break;
+                    default:
+                        memDump();
+                        exit(1);
+                }
                 break;
 
             default:
                 memDump();
-                exit(0);
+                exit(1);
         }
 
     }
 }
 
 void Chip8::memDump() {
-    const int width = 16;
     cout << "Memory Dump:" << endl;
 
-    for (int j = 0; j < 4096; j += width) {
+    for (int j = 0; j < 4096; j += 16) {
         cout << "0x" << setw(4) << setfill('0') << hex << j << " | ";
-        for (int k = 0; k < width; ++k) {
+        for (int k = 0; k < 16; ++k) {
             if (memory[j + k] == 0x00) {
                 cout << BLUE;
             } else if (j + k >= 0x0200 && memory[j+k] != 0x00) {
